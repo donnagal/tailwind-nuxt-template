bbacb80dc908399b39bd3ecf5f416202979c3840
Donna Galletta <donna.galletta@isobar.com>
2019-12-16 14:30:33 +1100

    add site

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   <nav>
+    <nuxt-link :to="{name: 'index'}">Home</nuxt-link>
+  </nav>
+  </div>
+</template>
+
+
+<script>
+</script>
+
+
+<style>
+  nav {
+    @apply w-full py-10 bg-gray-900 text-white text-center;
+  }
+</style>
